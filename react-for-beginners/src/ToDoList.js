0fb13e67e19a7b982de76da09916cdfe248692da
Door_km <156165073+MunKyungMan@users.noreply.github.com>
2024-09-20 16:39:29 +0900

    Add files via upload

---
+++ react-for-beginners/src/ToDoList.js
@@ -0,0 +1,37 @@
+import { useState, useEffect } from "react";
+
+function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [...currentArray, toDo]);
+  };
+  console.log(toDos);
+
+  return (
+    <div>
+      <h1>My To Do List ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((item, index) => (
+        <li key={index}>{item}</li>
+      ))}
+    </div>
+  );
+}
+
+export default App;
